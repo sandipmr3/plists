90ba7cf2828ff38b1ea043c7aac70517f618fba8
sandipmr3 <sandipmr3@gmail.com>
2024-02-13 17:26:03 +0530

    revert on 1.0.1 for plist version and fixed issue for try catch depricated

--- plists-master/test/make.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-erl -sname first_node@localhost \
--run file set_cwd .. \
--run make all \
--run file set_cwd test \
--pa ../ebin -run make all -run plists_unittests run \
--run init stop -noshell
